39aed4d84131d9c1eda93dd532253a0961e6a79a
ryanmbigelow <ryanmbigelow@gmail.com>
2022-10-31 20:17:17 -0500

    created 4 dummy arrays and assigned a teammate to each one to create the dummy data inside

---
+++ main.js
@@ -0,0 +1,16 @@
+//Arrays
+const pinnedRepoArray = [
+//Ryan takes this one
+]
+
+const repoArray = [
+//Barry takes this one
+]
+
+const projectsArray = [
+//Dro takes this one
+]
+
+const packagesArray = [
+//James takes this one
+]
