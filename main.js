450f08f7cc49436482d9327310bf2c06c8654b8e
dreax <gejuan.demby@gmail.com>
2022-10-31 20:39:52 -0500

    Added objects to 'projectsArray'

--- main.js
+++ main.js
@@ -7,8 +7,29 @@ const repoArray = [
 //Barry takes this one
 ]
 
-const projectsArray = [
+
+
 //Dro takes this one
+const projectsArray = [
+
+{
+  id:1,
+  repoName: "Example 1",
+  repoDescription: " ",
+},
+
+{
+  id:2,
+  repoName: "my-goals",
+  repoDescription: " ",
+},
+
+{
+  id:3,
+  repoName: "Sample My Goals",
+  repoDescription: "Goals for the NSS Bootcamp",
+}
+
 ]
 
 const packagesArray = [
