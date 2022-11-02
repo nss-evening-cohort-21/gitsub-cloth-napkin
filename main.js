db4a32f124c93bb7e0744b222e7c6ec42110a953
James <James.reinhardt1@gmail.com>
2022-11-01 20:20:08 -0500

    Added package cards and possibly package form

--- main.js
+++ main.js
@@ -164,11 +164,39 @@ const projectsOnDom =  () => {
   const domString = ``;
 }
 
-const packagesOnDom =  () => {
-  const domString = ``;
+const packagesOnDom = (array) => {
+  let domString = ``;
+  array.forEach((repo) => {
+    domString += `
+      <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">${repo.cardName}</h5>
+          <p class="card-text">${repo.description}</p>
+          <a href="#" class="btn btn-primary" type="submit">Learn More</a>
+        </div>
+      </div>
+    `
+  });
+  renderToDom('#packagesSelector', domString);
+}
+
+const packagesForm = () => {
+  let domString = `
+  <form id= "newProjectForm">
+    <div class="mb-3">
+      <label for="projectBoardInput1" class="form-label">Project board name</label>
+      <input type="text" class="form-control" id="projectBoardInput1" placeholder="Name here">
+    </div>
+    <div class="mb-3">
+      <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+      <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    </div>
+  </form>
+  `
 }
 
 const startApp = () => {
   pinnedRepoCardsOnDom(pinnedRepoArray);
+  packagesOnDom(packagesArray);
 }
 startApp();
